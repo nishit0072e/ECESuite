8d9321e87da455c2076752d40bb049d0f2ebd113
Nishit Bayen <65007263+nishit0072e@users.noreply.github.com>
2025-11-23 14:53:42 +0000

    Initial commit

---
+++ ece-suite/eslint.config.js
@@ -0,0 +1,16 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
