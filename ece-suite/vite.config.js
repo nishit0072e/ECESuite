d70e2493086ccc2bafe3b206166c93f2d4d3c72d
Nishit Bayen <65007263+nishit0072e@users.noreply.github.com>
2025-11-23 20:43:56 +0530

    Delete ece-suite directory

--- ece-suite/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
-export default defineConfig({
-  plugins: [react()],
-  base: '/ece-suite/', // <--- ADD THIS LINE (must match your repo name)
-})
